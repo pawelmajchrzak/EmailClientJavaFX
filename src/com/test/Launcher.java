1c62722118ea5c380282db08b94ea60866897e42
Pawel Majchrzak <pawel.majchrzak.programista@gmail.com>
2023-10-13 23:57:10 +0200

    createMainWindow

--- src/com/test/Launcher.java
+++ src/com/test/Launcher.java
@@ -15,9 +15,9 @@ public class Launcher extends Application {
     public void start(Stage primaryStage) throws Exception {
 
 
-        Parent parent = FXMLLoader.load(getClass().getResource("view/LoginWindow.fxml"));
+        Parent parent = FXMLLoader.load(getClass().getResource("view/MainWindow.fxml"));
 
-        Scene scene = new Scene(parent, 400, 340);
+        Scene scene = new Scene(parent);
         primaryStage.setX(-1150);
         primaryStage.setY(300);
         primaryStage.setScene(scene);

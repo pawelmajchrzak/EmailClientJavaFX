4b3d22281370795c87978379eda4b10e3d2490ae
Pawel Majchrzak <pawel.majchrzak.programista@gmail.com>
2023-10-20 23:08:52 +0200

    eventWhenNewMessageIncoming

--- src/com/test/EmailManager.java
+++ src/com/test/EmailManager.java
@@ -1,11 +1,18 @@
 package com.test;
 
 import com.test.controller.services.FetchFolderService;
+import com.test.controller.services.FolderUpdaterService;
 import com.test.model.EmailAccount;
 import com.test.model.EmailTreeItem;
 import javafx.scene.control.TreeItem;
 
+import javax.mail.Folder;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmailManager {
+
+    private FolderUpdaterService folderUpdaterService;
 
     // Folder handling:
     private EmailTreeItem<String> foldersRoot = new EmailTreeItem<String>("");
@@ -14,9 +21,19 @@ public class EmailManager {
         return foldersRoot;
     }
 
+    private List<Folder> folderList = new ArrayList<Folder>();
+    public List<Folder> getFolderList() {
+        return this.folderList;
+    }
+
+    public EmailManager() {
+        folderUpdaterService = new FolderUpdaterService(folderList);
+        folderUpdaterService.start();
+    }
+
     public void addEmailAccount(EmailAccount emailAccount) {
         EmailTreeItem<String> treeItem = new EmailTreeItem<String>(emailAccount.getAddress());
-        FetchFolderService fetchFolderService = new FetchFolderService(emailAccount.getStore(), treeItem);
+        FetchFolderService fetchFolderService = new FetchFolderService(emailAccount.getStore(), treeItem, folderList);
         fetchFolderService.start();
         foldersRoot.getChildren().add(treeItem);
     }
--- src/com/test/controller/services/FetchFolderService.java
+++ src/com/test/controller/services/FetchFolderService.java
@@ -8,15 +8,22 @@ import javafx.concurrent.Task;
 import javax.mail.Folder;
 import javax.mail.MessagingException;
 import javax.mail.Store;
+import javax.mail.event.MessageCountEvent;
+import javax.mail.event.MessageCountListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FetchFolderService extends Service<Void> {
 
     private Store store;
     private EmailTreeItem<String> foldersRoot;
 
-    public FetchFolderService(Store store, EmailTreeItem<String> foldersRoot) {
+    private List<Folder> folderList;
+
+    public FetchFolderService(Store store, EmailTreeItem<String> foldersRoot, List<Folder> folderList) {
         this.store = store;
         this.foldersRoot = foldersRoot;
+        this.folderList = folderList;
     }
 
     @Override
@@ -37,15 +44,31 @@ public class FetchFolderService extends Service<Void> {
 
     private void handleFolders(Folder[] folders, EmailTreeItem<String> foldersRoot) throws MessagingException {
         for(Folder folder: folders) {
+            folderList.add(folder);
             EmailTreeItem<String> emailTreeItem = new EmailTreeItem<String>(folder.getName());
             foldersRoot.getChildren().add((emailTreeItem));
             foldersRoot.setExpanded(true);
             fetchMessegesOnFolder(folder, emailTreeItem);
+            addMessageListenerToFolder(folder, emailTreeItem);
             if(folder.getType() == Folder.HOLDS_FOLDERS) {
                 Folder[] subFolder = folder.list();
                 handleFolders(subFolder, emailTreeItem);
             }
         }
+    }
+
+    private void addMessageListenerToFolder(Folder folder, EmailTreeItem<String> emailTreeItem) {
+        folder.addMessageCountListener(new MessageCountListener() {
+            @Override
+            public void messagesAdded(MessageCountEvent e) {
+                System.out.println("message added event!!!: " + e);
+            }
+
+            @Override
+            public void messagesRemoved(MessageCountEvent e) {
+                System.out.println("message added event!!!: " + e);
+            }
+        });
     }
 
     private void fetchMessegesOnFolder(Folder folder, EmailTreeItem<String> emailTreeItem) {
